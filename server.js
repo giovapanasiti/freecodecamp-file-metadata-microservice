cd267eb2b0570227d9b82d2e6c6cf38650ca151c
Glitch (zealous-reading) <none>
2018-07-20 10:49:18 +0000

    🍞🎲 Checkpoint ./server.js:383348/1671 ./views/index.html:383348/139

--- server.js
+++ server.js
@@ -5,28 +5,7 @@ var cors = require('cors');
 var multer = require('multer');
 var log = require('npmlog');
 var fs = require('fs');
-// File Upload related stuff
-var storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function(req, file, cb) {
-    var getFileExt = function(fileName) {
-      var fileExt = fileName.split(".");
-      if (fileExt.length === 1 || (fileExt[0] === "" && fileExt.length === 2)) {
-        return "";
-      }
-      return fileExt.pop();
-    };
-    cb(null, Date.now() + '.' + getFileExt(file.originalname));
-  }
-});
-
-var multerUpload = multer({
-  storage: storage
-});
-
-var uploadFile = multerUpload.single('upfile');
+var upload=multer({dest:"uploads/"});
 
 var app = express();
 
@@ -58,36 +37,12 @@ app.get('/hello', function(req, res){
 });
 
 
-app.post('/api/fileanalyse', function(req, res) {
-    uploadFile(req, res, function(err) {
-      
-      console.log(req);
-      
-      
-      if (err) {
-        // An error occurred when uploading 
-        log.error(err);
-      }
-      // Everything went fine 
-      var fileDetails = {
-        name: req.upfile.originalname,
-        size: req.upfile.size,
-        date: new Date().toLocaleString(),
-        file: req.upfile.filename
-      };
-      // save file to db
-      var file = new File(fileDetails);
-      file.save(function(err, file) {
-        if (err) {
-          log.error(err);
-          throw err;
-        }
-        log.info('Saved', file);
-      });
-      var filePath = "./uploads/" + req.file.filename; 
-      fs.unlinkSync(filePath);
-      res.send(fileDetails);
-    });
+app.post('/upload', upload.single("upfile"), function(req, res) {
+  
+    console.log(req.file)
+    
+    if 
+
   });
 
 
