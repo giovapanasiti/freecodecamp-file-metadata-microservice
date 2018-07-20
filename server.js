90ced9466bbd47a22fd91a170f337c55da16cc9d
Glitch (zealous-reading) <none>
2018-07-20 10:17:47 +0000

    👜☝️ Checkpoint ./server.js:383348/1477 ./package.json:383348/1951

--- server.js
+++ server.js
@@ -2,8 +2,29 @@
 
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 
-// require and use "multer"...
+
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    var getFileExt = function(fileName) {
+      var fileExt = fileName.split(".");
+      if (fileExt.length === 1 || (fileExt[0] === "" && fileExt.length === 2)) {
+        return "";
+      }
+      return fileExt.pop();
+    };
+    cb(null, Date.now() + '.' + getFileExt(file.originalname));
+  }
+});
+
+var multerUpload = multer({
+  storage: storage
+});
+var uploadFile = multerUpload.single('userFile');
 
 var app = express();
 
@@ -19,7 +40,31 @@ app.get('/hello', function(req, res){
 });
 
 app.post('/api/fileanalyse', function(req, res) {
-  console.log(req)
+  uploadFile(req, res, function(err) {
+      if (err) {
+        // An error occurred when uploading 
+        res.send(err);
+      }
+      // Everything went fine 
+      var fileDetails = {
+        name: req.file.originalname,
+        size: req.file.size,
+        date: new Date().toLocaleString(),
+        file: req.file.filename
+      };
+      // save file to db
+      // var file = new File(fileDetails);
+      // file.save(function(err, file) {
+      //   if (err) {
+      //     log.error(err);
+      //     throw err;
+      //   }
+      //   log.info('Saved', file);
+      // });
+      // var filePath = "./uploads/" + req.file.filename; 
+      // fs.unlinkSync(filePath);
+      res.send(fileDetails);
+    });
 })
 
 app.listen(process.env.PORT || 3000, function () {
